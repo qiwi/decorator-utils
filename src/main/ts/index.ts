e918d0f84eb52f85da5be495a6fe56f2ad9fa965
Anton Golub <antongolub@antongolub.com>
2022-12-06 14:08:51 +0300

    style: apply eslint-config-qiwi@2

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,9 +1,7 @@
 /** @module @qiwi/decorator-utils */
 
-import { constructDecorator } from './decorator'
-
 export { injectMeta } from './meta'
 export * from './resolver'
 export * from './decorator'
 
-export default constructDecorator
+export {constructDecorator as default} from './decorator'
